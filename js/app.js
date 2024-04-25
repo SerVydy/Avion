9479d229ca2ac294b9665e5397d06c9a72d626c7
SerVydy <srvydy@gmail.com>
2024-04-25 09:04:49 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const burger = () => {
+   const btn = document.querySelector('.burger svg:last-child')
+   const off = document.querySelector('.offset')
+   const close = document.querySelector('.offset nav svg')
+   btn.addEventListener('click', () => {
+      off.style.display = 'block'
+   })
+
+   close.addEventListener('click', () => {
+      off.style.display = 'none'
+   })
+}
+
+burger()
